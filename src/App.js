033ae9eb6858c78e3205d681b7dfb53d348bb260
heywon0909 <hw7319@naver.com>
2023-05-30 21:51:41 +0900

    짝맞추기 게임 만들기

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return <div className='w-full h-screen relative'>
+    <div className='bg-slate-600 absolute left-0 right-0 top-0 bottom-0 w-6/4 h-2/3 m-auto'>
+      hi
+    </div>
+  </div>;
+}
+
+export default App;
