c7677f24a7f01ba9b56db2eb67d355ccb171d596
heywon0909 <hw7319@naver.com>
2023-05-30 22:09:52 +0900

    게임 section 만드는중..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return <div className='w-full h-screen relative'>
-    <div className='bg-slate-600 absolute left-0 right-0 top-0 bottom-0 w-6/4 h-2/3 m-auto'>
+    <div className='flex flex-col justify-between bg-brand absolute left-0 right-0 top-0 bottom-0 w-5/6 h-5/6 m-auto border-8 border-neutral-400'>
+      <div className='w-full h-16 bg-neutral-400'>hi</div>
       hi
+      <div className='w-full h-16 bg-neutral-400'>hi</div>
     </div>
   </div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand:'#303135'
+      }
+    },
   },
   plugins: [],
 }
