461b06003bb1db2698195153fa11b2524802b822
hw7319 <hw7319@naver.com>
2023-05-31 14:55:05 +0900

    카드 맞추면 지우는 메서드 추가

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,10 @@
-import React, { useState } from "react";
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  useRef,
+} from "react";
 import CardItem from "./CardItem";
 
 function getNumber() {
@@ -38,33 +44,65 @@ function getRadomArr(numbers) {
 }
 export default function Board() {
   const [boardArr, setBoardArr] = useState(() => getRadomArr(getNumber()));
-  console.log("board", boardArr);
-  const [matchArr, setMatchArr] = useState([]);
-  const onClick = (itemObj) => {
-    setMatchArr((prevArr) => [...prevArr, itemObj]);
+
+  const [cardArr, setCardArr] = useState([]);
+  const [result, setResult] = useState("짝을 맞춰주세요");
+  const timeout = useRef(null);
+
+  const matchCardArr = useMemo(() => cardArr, [cardArr]);
+  const onCheckCardMatching = useCallback(() => {
+    if (matchCardArr.length === 2) {
+      const [card_1, card_2] = matchCardArr;
+      if (card_1.card === card_2.card) {
+        console.log("맞았습니다.");
+        setBoardArr((prevArr) => {
+          const newBoardArr = [...prevArr];
+          newBoardArr[card_1.row][card_1.col] = null;
+          newBoardArr[card_2.row][card_2.col] = null;
+          return newBoardArr;
+        });
+      } else {
+        console.log("틀렸습니다.");
+      }
+      timeout.current = setTimeout(() => {
+        setCardArr([]);
+      }, 500);
+    }
+  }, [cardArr]);
+
+  useEffect(() => {
+    onCheckCardMatching();
+  }, [matchCardArr]);
+
+  const onClick = (cardObj) => {
+    // 1. matchArr의 길이가 2를 넘어가면 에러 던져주기
+    // 2. matchArr 의 길이가 2가 되면 두 객체의 item 값 비교
+    // 3. 맞으면 board 배열에서 제외
+
+    if (matchCardArr.length === 2) return;
+    setCardArr((prevCardArr) => [...prevCardArr, cardObj]);
   };
-  const isMatchItem = (item, row, col) => {
-    return Object.values(matchArr).filter(
-      (obj) => obj.item === item && obj.row === row && obj.col === col
+  const isMatchCard = (card, row, col) => {
+    return Object.values(cardArr).filter(
+      (obj) => obj.card === card && obj.row === row && obj.col === col
     )?.length > 0
       ? true
       : false;
     return false;
   };
   return (
-    <div className="w-full grid grid-rows-4 grid-flow-col gap-1 place-content-center">
+    <div className="w-full p-2 grid grid-rows-4 grid-flow-col md:gap-2 gap-4 place-content-center">
       {boardArr &&
         boardArr.map((board, row) => {
-          return board.map((item, col) => {
-            console.log("isMatchItem", isMatchItem(item, row, col));
+          return board.map((card, col) => {
             return (
               <CardItem
-                key={`카드번호${item}배열${row}${col}`}
-                item={item}
+                key={`카드번호${card}배열${row}${col}`}
+                card={card}
                 onClick={onClick}
                 row={row}
                 col={col}
-                isMatch={isMatchItem(item, row, col) === true ? true : false}
+                isMatch={isMatchCard(card, row, col) === true ? true : false}
               />
             );
           });
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 
-export default function CardItem({ item, row, col, onClick, isMatch }) {
+export default function CardItem({ card, row, col, onClick, isMatch }) {
   const onClickItem = () => {
-    console.log("item", item);
-    onClick({ item, row, col });
+    onClick({ card, row, col });
   };
   return (
     <div
       className={
         isMatch
           ? "w-28 h-32 bg-slate-50 flex flex-col justify-center items-center border-4 border-blue-400 shadow-indigo-500/40"
-          : "w-28 h-32 bg-slate-50 flex flex-col justify-center items-center hover:border-4 hover:border-blue-400 hover:shadow-indigo-500/40"
+          : "w-28 h-32 bg-slate-50 flex flex-col justify-center items-center hover:border-4 hover:border-blue-400 "
       }
       onClick={onClickItem}
     >
-      <img
-        src={process.env.PUBLIC_URL + `/assets/image/${item}.png`}
-        alt="picture"
-      />
+      {card != null && (
+        <img
+          src={process.env.PUBLIC_URL + `/assets/image/${card}.png`}
+          alt="picture"
+        />
+      )}
     </div>
   );
 }
