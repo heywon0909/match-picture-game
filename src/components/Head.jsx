35da90dc68f0a978e5e5c060f3f1ec049024473b
hw7319 <hw7319@naver.com>
2023-05-31 18:04:54 +0900

    time 셋팅 추가

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -8,31 +8,24 @@ export default function Head() {
     timeout.current = setInterval(() => {
       setTime((prevTime) => {
         const newSec = prevTime.sec === 59 ? 0 : prevTime.sec + 1;
-        const newMin = prevTime;
-      });
-      setSec((prev) => {
-        if (prev === 60) return 0;
-        else return prev + 1;
-      });
-      setMin((prev) => {
-        if (Number(sec) === 60) {
-          return prev + 1;
-        } else return prev;
+        const newMin = prevTime.sec === 59 ? prevTime.min + 1 : prevTime.min;
+        return { min: newMin, sec: newSec };
       });
     }, 1000);
     return () => clearInterval(timeout.current);
-  }, [sec]);
+  }, [time]);
 
-  const timeSetting = () => {
-    const fmtMin = min < 10 ? "0" + min : min;
-    const fmtSec = sec < 10 ? "0" + sec : sec;
-    return fmtMin + ":" + fmtSec;
+  const timeSetting = (time) => {
+    return time < 10 ? "0" + time : time;
   };
+
+  const timeString = `${timeSetting(time.min)}:${timeSetting(time.sec)}`;
+
   return (
     <div className="w-full h-16 bg-neutral-400">
       <div className="flex w-2/3 bg-neutral-100 rounded-md absolute top-1">
         <div className="flex p-2 w-20 text-lg justify-center items-center font-semibold">
-          <p>{timeSetting()}</p>
+          <p>{timeString}</p>
         </div>
       </div>
     </div>
