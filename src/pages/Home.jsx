1a5324f1c14cdeaa35fd496ccaa646f7dc84482e
hw7319 <hw7319@naver.com>
2023-06-01 10:16:10 +0900

    start 화면 UI 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         backgroundImage: `url(${process.env.PUBLIC_URL}/assets/image/start.jpg)`,
       }}
     >
-      <div className="flex w-full justify-center items-center">
+      <div className="flex flex-col w-full justify-center items-center">
         <div className="flex space-x-6">
           <h1 className="text-8xl text-gray-50 font-extrabold tracking-widest">
             G
@@ -23,6 +24,13 @@ export default function Home() {
             E
           </h1>
         </div>
+        <Link to="/game/start">
+          <div className="flex relative top-8">
+            <button className="w-64 h-10 bg-white text-red-500 font-extrabold text-2xl rounded-md animate-bounce">
+              start
+            </button>
+          </div>
+        </Link>
       </div>
     </div>
   );
