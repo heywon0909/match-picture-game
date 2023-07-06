fc6079e2ed8adfaf4615ddcadbb00c6c6fce11be
heywon0909 <hw7319@naver.com>
2023-07-07 08:20:13 +0900

    typescript 셋팅 설정 완료

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+const path = require("path");
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+  name: "match-picture-game",
+  mode: "development",
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".jsx", ".js", ".ts", ".tsx"],
+  },
+  entry: {
+    app: "./src/index",
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      options: { plugins: ['react-refresh/babel'] },
+    }, {
+      test: /\.tsx?$/,
+      loader: 'ts-loader',
+      exclude: path.join(__dirname, 'node_modules'),
+    }],
+  },
+  plugins: [
+    new ReactRefreshPlugin(),
+    new ForkTsCheckerWebpackPlugin()
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/dist",
+  },
+  devServer: {
+    devMiddleware: { publicPath: "/dist" },
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+  },
+};
+export default config;
